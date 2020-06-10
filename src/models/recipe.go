15e9809085bd07847ccca7cc308086136997ba0f
Finn Moritz Bätzel <moritz@enwork.de>
2020-06-10 18:37:52 +0200

    Started to cleanup

--- src/models/recipe.go
+++ src/models/recipe.go
@@ -1,5 +1,6 @@
 package models
 
+// A Recipe models a single step when executing a template.
 type Recipe struct {
 	Template    string `json:"template"`
 	Destination string `json:"destination"`
--- src/services/load_template.go
+++ src/services/load_template.go
@@ -12,28 +12,48 @@ import (
 	"strings"
 )
 
+// Load a template by its path
 func LoadTemplate(templateRoot, destination string, args []string) {
+	checkTemplateFolder(templateRoot)
+
+	variableNames := getVariables(templateRoot)
+	recipes := getRecipes(templateRoot)
+
+	variables := defineVariables(variableNames, args)
+
+	for _, r := range recipes {
+		ProcessRecipe(templateRoot, destination, r, variables)
+	}
+}
+
+// Check if the given template exists.
+func checkTemplateFolder(templateRoot string) {
 	_, err := os.Stat(templateRoot)
 	if err != nil {
 		log.Fatalf("Failed to load template: %s\n", templateRoot)
 	}
+}
 
+func getVariables(templateRoot string) []string {
 	variablesPath := path.Join(templateRoot, "_variables.json")
-	recipePath := path.Join(templateRoot, "_recipes.json")
-
 	variablesContent, err := ioutil.ReadFile(variablesPath)
 	if err != nil {
 		log.Fatalf("Template %s has no variables defined\n", templateRoot)
 	}
+	var variables []string
+	err = json.Unmarshal([]byte(variablesContent), &variables)
+	if err != nil {
+		log.Fatalf("Variables of %s could not be loaded\n", templateRoot)
+	}
+	return variables
+}
+
+func getRecipes(templateRoot string) []models.Recipe {
+	recipePath := path.Join(templateRoot, "_recipes.json")
+
 	recipeContent, err := ioutil.ReadFile(recipePath)
 	if err != nil {
 		log.Fatalf("Template %s has no recipes defined\n", templateRoot)
-	}
-
-	var variableNames []string
-	err = json.Unmarshal([]byte(variablesContent), &variableNames)
-	if err != nil {
-		log.Fatalf("Variables of %s could not be loaded\n", templateRoot)
 	}
 
 	var recipes []models.Recipe
@@ -41,7 +61,10 @@ func LoadTemplate(templateRoot, destination string, args []string) {
 	if err != nil {
 		log.Fatalf("Recipes of %s could not be loaded\n", templateRoot)
 	}
+	return recipes
+}
 
+func defineVariables(variableNames []string, args []string ) map[string]string {
 	variables := map[string]string{}
 	reader := bufio.NewReader(os.Stdin)
 
@@ -56,7 +79,5 @@ func LoadTemplate(templateRoot, destination string, args []string) {
 		}
 	}
 
-	for _, r := range recipes {
-		ProcessRecipe(templateRoot, destination, r, variables)
-	}
+	return variables
 }
